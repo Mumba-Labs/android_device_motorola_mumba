62fc654b07ce532f393b49313928b8cbe617a04d
zaraknyu <razenxizu@gmail.com>
2026-07-11 19:12:34 +0000

    mumba:  Build clearkey DRM HAL

--- device.mk
+++ device.mk
@@ -145,6 +145,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     MotoEuicc
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm-service.clearkey
+
 # Fastboot
 PRODUCT_PACKAGES += \
     fastbootd
