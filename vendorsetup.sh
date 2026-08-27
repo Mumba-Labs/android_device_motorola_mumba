50ad3b2e9b9e33df858fea589793a92f2703296d
n0escape-exe <viaan4542@gmail.com>
2026-08-27 11:33:52 +0000

    mumba: Build GameBar
    * Add it to vendorsetup.sh as well
    
    Signed-off-by: n0escape-exe <viaan4542@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -13,3 +13,7 @@ git clone https://github.com/Mumba-Labs/android_hardware_motorola.git hardware/m
 # Dolby Atmos
 rm -rf hardware/dolby
 git clone https://github.com/Mumba-Labs/android_hardware_dolby.git hardware/dolby
+
+# GameBar
+rm -rf packages/apps/GameBar
+git clone https://github.com/Larry-ROM-Archive/packages_apps_GameBar packages/apps/GameBar
