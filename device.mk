cdd2af6a24a80dc4ced04b5a9056654f513ae22b
n0escape-exe <viaan4542@gmail.com>
2026-08-30 13:23:10 +0000

    mumba: Build GameBar
    Signed-off-by: n0escape-exe <viaan4542@gmail.com>

--- device.mk
+++ device.mk
@@ -11,6 +11,9 @@ $(call inherit-product, hardware/qcom-caf/common/common.mk)
 # Google Camera
 $(call inherit-product-if-exists, hardware/GoogleCamera/GoogleCamera.mk)
 
+# GameBar Performance Overlay
+$(call inherit-product, packages/apps/GameBar/gamebar.mk)
+
 # A/B
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
